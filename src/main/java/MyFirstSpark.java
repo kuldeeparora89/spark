9dcc8ca9113cca6dc38f92ef96a8745f7b9a18df
Kd Singh <kuldeeparora89@gmail.com>
2017-03-31 20:10:38 +0530

    First project

---
+++ src/main/java/MyFirstSpark.java
@@ -0,0 +1,33 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by kuldeep pc on 31-Mar-17.
+ */
+public class MyFirstSpark {
+
+    public static void main(String[] args) {
+
+
+
+
+        String master = args[0]; 
+        SparkConf conf = new SparkConf().setMaster(master).setAppName("Simple Application");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+        JavaRDD<Integer> distData = sc.parallelize(data);
+
+        System.out.println("Count  ------> " + distData.count());
+
+
+
+
+        sc.stop();
+
+
+    }
+}
